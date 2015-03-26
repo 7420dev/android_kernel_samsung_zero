ae0f4e787e5ac720d04adaec0909f7bc953a0eba
Vinayak Menon <vinmenon@codeaurora.org>
2015-03-26 15:38:55 +0530

    Revert "vmstat: create separate function to fold per cpu diffs into local counters"
    This reverts commit 1ba8ad85c165abc0fdbc240cd77162ed261acf10.
    
    This patch is one among the 6 patches that were initially picked
    to fix an issue where tasks were getting blocked in reclaim path.
    But these patches are found to cause cpu wakeups.
    
    Change-Id: I7fca0bd378d6183dfc0f8bc302397d03d04fe865
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -5408,7 +5408,7 @@ static int page_alloc_cpu_notify(struct notifier_block *self,
 		 * This is only okay since the processor is dead and cannot
 		 * race with what we are doing.
 		 */
-		cpu_vm_stats_fold(cpu);
+		refresh_cpu_vm_stats(cpu);
 	}
 	return NOTIFY_OK;
 }
