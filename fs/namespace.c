f1d6eeb3609f572c000a58d5fef784e9b3c06b4f
arter97 <qkrwngud825@gmail.com>
2015-07-05 03:41:58 +0900

    Revert "fs: sync: Asynchronous Fsync from HTC"
    This reverts commit 8125c532b02a7d8c36d39f18e4ffcce2180e26cc.
    
    Ah, yes, Galaxy S6 uses UFS instead of eMMC.
    
    Signed-off-by: arter97 <qkrwngud825@gmail.com>
    
    Conflicts:
    	fs/Kconfig
    	fs/sync.c
    	fs/namespace.c

--- fs/namespace.c
+++ fs/namespace.c
@@ -2059,15 +2059,6 @@ static int do_new_mount(struct path *path, const char *fstype, int flags,
 	err = do_add_mount(real_mount(mnt), path, mnt_flags);
 	if (err)
 		mntput(mnt);
-#ifdef CONFIG_ASYNC_FSYNC
-	if (!err && ((((!strcmp(fstype, "ext4")) || (!strcmp(fstype, "f2fs"))) &&
-	    !strcmp(path->dentry->d_name.name, "data")) ||
-	    (!strcmp(fstype, "fuse") &&
-	    !strcmp(path->dentry->d_name.name, "emulated")))) {
-                mnt->mnt_sb->fsync_flags |= FLAG_ASYNC_FSYNC;
-		pr_info("namespace: enabled FLAG_ASYNC_FSYNC\n");
-	}
-#endif
 	return err;
 }
 
