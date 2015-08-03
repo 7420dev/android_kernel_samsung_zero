df005e09ff5a650ad272bf56f3aea8efa63f011c
Park Ju Hyung <qkrwngud825@gmail.com>
2015-08-03 13:02:32 +0900

    kernelzip: switch to shell script & create swapfile
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build_master.sh
+++ build_master.sh
@@ -21,7 +21,6 @@ if [ -e recovery.img ] ; then
 	rm arter97-recovery-"$(cat version)"-philz_touch_"$(cat version_recovery | awk '{print $1}')".zip 2>/dev/null
 	cp recovery.img recoveryzip/
 	cd recoveryzip/
-	cp ../kernelzip/META-INF/com/google/android/update-binary META-INF/com/google/android/update-binary
 	sed -i -e s/PHILZ_VERSION/$(cat ../version_recovery | awk '{print $1}')/g -e s/CWM_VERSION/$(cat ../version_recovery | awk '{print $2 }')/g META-INF/com/google/android/updater-script
 	7z a -mx9 arter97-recovery-"$(cat ../version)"-philz_touch_"$(cat ../version_recovery | awk '{print $1}')"-tmp.zip *
 	zipalign -v 4 arter97-recovery-"$(cat ../version)"-philz_touch_"$(cat ../version_recovery | awk '{print $1}')"-tmp.zip ../arter97-recovery-"$(cat ../version)"-philz_touch_"$(cat ../version_recovery | awk '{print $1}')".zip
@@ -29,6 +28,5 @@ if [ -e recovery.img ] ; then
 	sed -i -e s/$(cat ../version_recovery | awk '{print $1}')/PHILZ_VERSION/g -e s/$(cat ../version_recovery | awk '{print $2 }')/CWM_VERSION/g META-INF/com/google/android/updater-script
 	cd ..
 	ls -al arter97-recovery-"$(cat version)"-philz_touch_"$(cat version_recovery | awk '{print $1}')".zip
-	rm recoveryzip/META-INF/com/google/android/update-binary
 	rm recoveryzip/recovery.img
 fi
