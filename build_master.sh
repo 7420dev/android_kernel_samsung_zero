7ec3d630424f7e51e37d081e2efec9a3068a87a0
Park Ju Hyung <qkrwngud825@gmail.com>
2015-08-03 13:23:25 +0900

    build_master: record build branch on the filename
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build_master.sh
+++ build_master.sh
@@ -9,11 +9,11 @@ if [ -e boot.img ] ; then
 	rm arter97-kernel-"$(cat version)".zip 2>/dev/null
 	cp boot.img kernelzip/boot.img
 	cd kernelzip/
-	7z a -mx9 arter97-kernel-"$(cat ../version)"-tmp.zip *
-	zipalign -v 4 arter97-kernel-"$(cat ../version)"-tmp.zip ../arter97-kernel-"$(cat ../version)".zip
-	rm arter97-kernel-"$(cat ../version)"-tmp.zip
+	7z a -mx9 arter97-kernel-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat ../version)"-tmp.zip *
+	zipalign -v 4 arter97-kernel-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat ../version)"-tmp.zip ../arter97-kernel-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat ../version)".zip
+	rm arter97-kernel-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat ../version)"-tmp.zip
 	cd ..
-	ls -al arter97-kernel-"$(cat version)".zip
+	ls -al arter97-kernel-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat version)".zip
 	rm kernelzip/boot.img
 fi
 
