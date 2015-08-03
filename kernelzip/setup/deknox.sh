6408aaaab10f3de265daaeda232ce2ad5dfa4a5b
Park Ju Hyung <qkrwngud825@gmail.com>
2015-08-03 12:19:58 +0900

    kernelzip: remove Knox upon installation
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

---
+++ kernelzip/setup/deknox.sh
@@ -0,0 +1,28 @@
+#!/sbin/sh
+
+cd /system
+
+rm -rf *app/Bridge*
+rm -rf *app/ContainerAgent*
+rm -rf *app/ContainerEventsRelayManager*
+rm -rf *app/DiagMonAgent*
+rm -rf *app/ELMAgent*
+rm -rf *app/FotaClient*
+rm -rf *app/FWUpgrade*
+rm -rf *app/HLC*
+rm -rf *app/KLMSAgent*
+rm -rf *app/Knox*
+rm -rf *app/KNOX*
+rm -rf *app/LocalFOTA*
+rm -rf *app/RCPComponents*
+rm -rf *app/SecKids*
+rm -rf *app/SecurityLogAgent*
+rm -rf *app/SPDClient*
+rm -rf *app/UniversalMDMClient*
+rm -rf container/Knox*
+
+sed -i -e '/ro.securestorage.knox/c\ro.securestorage.knox=false' build.prop
+sed -i -e '/ro.securestorage.support/c\ro.securestorage.support=false' build.prop
+sed -i -e '/ro.config.timaversion/d' build.prop
+sed -i -e '/ro.config.tima/c\ro.config.tima=0' build.prop
+sed -i -e '/ro.config.knox/c\ro.config.knox=0' build.prop
