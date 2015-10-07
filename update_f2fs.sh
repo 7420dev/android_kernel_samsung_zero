dd053864949a2531deaeb7eb29e0853627ead14e
Park Ju Hyung <qkrwngud825@gmail.com>
2015-10-07 20:45:29 +0900

    update_f2fs: regenerate
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- update_f2fs.sh
+++ update_f2fs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 git fetch f2fs
 
-find . -not -path '*/\.*' -name '*f2fs*' | grep -v 'update_f2fs\|recovery\|ramdisk' | while read f2fs; do rm -rf $f2fs; git checkout 6282adbf932c226f76e1b83e074448c79976fe75 -- $f2fs; done
+find . -not -path '*/\.*' -name '*f2fs*' | grep -v 'update_f2fs\|recovery\|ramdisk' | while read f2fs; do rm -rf $f2fs; git checkout torvalds/master -- $f2fs; done
 
 rm -rf fs/f2fs
-git checkout 6282adbf932c226f76e1b83e074448c79976fe75 -- fs/f2fs
+git checkout torvalds/master -- fs/f2fs
 
-git cherry-pick --no-commit ede50acb557df1ef707295efa33a85b1da8bba35
-git cherry-pick --no-commit 5d8c190f0beb972f78e2904755d3940c9f6babbf
-git cherry-pick --no-commit 925f2aaaf04e2ff29f4d10f35e68c54b9feacc3c
+git cherry-pick --no-commit a330303d700a8e5ead2c8af08e58ac7a212fbc9d
+git cherry-pick --no-commit ed44568d272a322d5dc272ceed5259b58b203ebc
+git cherry-pick --no-commit 5562ccf9c1b3506f7aa7f961071582adde06a6d2
 git status
