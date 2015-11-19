7e86d2f5e1aed7defadc41b1bf39b1a014decf1f
Park Ju Hyung <qkrwngud825@gmail.com>
2015-11-19 22:45:47 +0900

    releasetools: make sure default.prop's override is properly passed
    /system/default.prop has higher priority than /system/build.prop.
    
    This commit copies /default.prop's content starting from OVERRIDE to
    /system/default.prop.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build_master.sh
+++ build_master.sh
@@ -8,6 +8,7 @@ fi
 if [ -e boot.img ] ; then
 	rm arter97-kernel-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat version)".zip 2>/dev/null
 	cp boot.img kernelzip/boot.img
+	tail -n $(($(cat ramdisk/default.prop | wc -l) - $(grep -n "START OVERRIDE" ramdisk/default.prop | cut -d : -f 1) + 1)) ramdisk/default.prop > kernelzip/default.prop
 	cd kernelzip/
 	7z a -mx9 arter97-kernel-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat ../version)"-tmp.zip *
 	zipalign -v 4 arter97-kernel-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat ../version)"-tmp.zip ../arter97-kernel-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat ../version)".zip
