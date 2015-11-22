d6c60adcb39aab3ee1ee510ee9dc86b2a908dbaa
Park Ju Hyung <qkrwngud825@gmail.com>
2015-11-22 21:53:49 +0900

    dm_cpu_hotplug: allow all A53 cores to be turned on during screen-off
    This fixes music studderings and VoIP app issues.
    Seems like 2 A53 cores weren't enough.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- ramdisk/init.prop.sh
+++ ramdisk/init.prop.sh
@@ -14,9 +14,9 @@ if [[ $(cat /data/media/0/arter97/fade_led) == "0" ]]; then
 	echo "0" > /sys/devices/virtual/sec/led/led_notification_ramp_control
 fi
 
-if [[ $(cat /data/media/0/arter97/dualcore_blank) == "0" ]]; then
-	echo "Disabling dualcore_blank mode ..."
-	echo "3" > /sys/power/enable_dm_hotplug
+if [[ $(cat /data/media/0/arter97/dualcore_blank) == "1" ]]; then
+	echo "Enabling dualcore_blank mode ..."
+	echo "2" > /sys/power/enable_dm_hotplug
 fi
 if [[ $(cat /data/media/0/arter97/dualcore_blank) == "2" ]]; then
 	echo "Allowing all CPUs to be turned on during screen-off ..."
