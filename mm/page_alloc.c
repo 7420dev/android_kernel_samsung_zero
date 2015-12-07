dba321262cbc15dc2d1ea2feee2709c2b9241b8b
Park Ju Hyung <qkrwngud825@gmail.com>
2015-12-07 17:54:32 +0900

    Revert "mm: Add min_free_order_shift tunable."
    This reverts commit 6df890364b72edc57fb74aa0f59668450fa678ff.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -202,7 +202,6 @@ static char * const zone_names[MAX_NR_ZONES] = {
 };
 
 int min_free_kbytes = 1024;
-int min_free_order_shift = 1;
 
 static unsigned long __meminitdata nr_kernel_pages;
 static unsigned long __meminitdata nr_all_pages;
@@ -1733,7 +1732,7 @@ static bool __zone_watermark_ok(struct zone *z, int order, unsigned long mark,
 		free_pages -= z->free_area[o].nr_free << o;
 
 		/* Require fewer higher order pages to be free */
-		min >>= min_free_order_shift;
+		min >>= 1;
 
 		if (free_pages <= min)
 			return false;
